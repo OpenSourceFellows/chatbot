85c78ed311beda353ee60f52c0a5c5e9062953e9
Abdullah Durrani <134801570+bootupAbdullah@users.noreply.github.com>
2025-07-08 16:21:51 -0400

    Refactor: use config module for Twilio credentials in send-test-sms.js

--- send-test-sms.js
+++ send-test-sms.js
@@ -1,15 +1,12 @@
-require('dotenv').config();
 const twilio = require('twilio');
+const { twilioAccountSID, twilioAuthToken, twilioPhoneNumber } = require('./src/config/config');
 
-const client = twilio(
-  process.env.TWILIO_ACCOUNT_SID,
-  process.env.TWILIO_AUTH_TOKEN
-);
+const client = twilio(twilioAccountSID, twilioAuthToken);
 
 client.messages
   .create({
     body: 'Test Message: Abdullah, Project Earth, today is June 9th, time is 5:22 PM EST',
-    from: process.env.TWILIO_PHONE_NUMBER,
+    from: twilioPhoneNumber,
     to: '+18777804236'
   })
   .then(message => {
--- src/server.js
+++ src/server.js
@@ -2,13 +2,12 @@ const express = require('express');
 const httpStatus = require('http-status');
 const morgan = require('./config/morgan');
 const { errorHandler, errorConverter } = require('./middlewares/error');
+const authRoutes = require('./routes/auth.routes');
+const chatbotRoutes = require('./routes/chatbot.routes');
+const edxRoutes = require('./routes/edx.routes');
+const messagingRoutes = require('./routes/messaging.routes');
 const ApiError = require('./utils/ApiError');
 
-
-const authRoutes = require('./routes/auth.routes')
-const chatbotRoutes = require('./routes/chatbot.routes')
-const edxRoutes = require('./routes/edx.routes')
-const messagingRoutes = require('./routes/messaging.routes')
 const app = express();
 
 app.use(morgan.successHandler);
@@ -19,7 +18,6 @@ app.use('/auth',authRoutes);
 app.use('/chatbot',chatbotRoutes);
 app.use('/edx', edxRoutes);
 app.use('/messaging', messagingRoutes);
-
 
 app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
