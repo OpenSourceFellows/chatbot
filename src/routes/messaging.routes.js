270c7163c3479e4d7fd71ba25c18aa784689d709
Yori! <adeyori.adekunle@bulldogs.aamu.edu>
2025-07-13 06:54:49 -0500

    resloved till issue 2.2

--- src/routes/messaging.routes.js
+++ src/routes/messaging.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('📨 Messaging API is working');
+  res.send('Messaging API is working');
 });
 
 module.exports = router;
