1f8c780a25341c245e317a156c03cebedd71a38f
Mark de Dios <mhdedios@gmail.com>
2025-07-13 17:12:56 -0700

    remove twilio incoming message validation from validations index

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,3 +1,2 @@
 module.exports.envValidation = require('./env.validation');
 module.exports.incomingMessageValidation = require('./webhook/incoming.message.validation');
-module.exports.twilioIncomingMessageValidation = require('./twilio/incoming.message.validation');
