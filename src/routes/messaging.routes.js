d2cf66b6e623bc3095a8514a26f7d6361e9e3d60
Mark de Dios <mhdedios@gmail.com>
2025-07-13 17:10:35 -0700

    delete import for twilio incoming message validation, replace with general

--- src/routes/messaging.routes.js
+++ src/routes/messaging.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const { messagingController } = require('../controllers');
 const validate = require('../middlewares/validate');
-const { twilioIncomingMessageValidation } = require('../validations');
+const { incomingMessageValidation } = require('../validations');
 
 const router = express.Router();
 
-router.post('/webhook/incoming-message', validate(twilioIncomingMessageValidation), messagingController.handleIncomingMessage);
+router.post('/webhook/incoming-message', validate(incomingMessageValidation), messagingController.handleIncomingMessage);
 
 router.get('/', (req, res) => {
   res.send('📨 Messaging API is working');
